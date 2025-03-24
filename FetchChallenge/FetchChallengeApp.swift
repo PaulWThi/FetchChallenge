4fbfe4945d6077751f2e9099b9f0630980a20f57
Paul Thi <paul.w.thi@gmail.com>
2025-03-24 17:50:43 -0500

    Initial Commit

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Paul Thi on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
